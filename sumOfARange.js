232408b1932e7e369eb7a9afa89c8b787ee492ef
Jacob Gramer <grameraggie@gmail.com>
2022-12-20 09:27:13 -0600

    Adds buildGraph function to convert array of roads into a graph that contains all connected nodes.

--- sumOfARange.js
+++ sumOfARange.js
@@ -1,17 +1,17 @@
 const range = (start, end, step = 1) => {
-    let range = [];
-    for (let i=start; i<=end; i+=step) {
-        range.push(i);
-    }
-    return range;
-}
+  let range = [];
+  for (let i = start; i <= end; i += step) {
+    range.push(i);
+  }
+  return range;
+};
 
 const sum = (array) => {
-    let total = 0;
-    for (let i=0; i<array.length; i++) {
-        total += array[i];
-    }
-    return total;
-}
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+};
 
-console.log(sum(range(0,33)));
+console.log(sum(range(0, 33)));
